4c2242c97f372085ecdbc30375d4ef63a04f8683
Sandrine IBRAIMA-NOMBO <sandrineibraima@gmail.com>
2023-08-18 19:07:33 +0200

    update du fichier
    modification de la valeur initiale de $index et correction de l'appel à la fonction getClueString

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once './lib/debug.php';
 // VARIABLES NÉCESSAIRES
 $lettre = ' ';       // LA LETTRE QUI VIENT D'ÊTRE SAISIE PAR L'UTILISATEUR (SI PAS LE PREMIER ROUND)
 $propositions   = '';       // CHAÎNE DE CARACTÈRES REGROUPANT L'ENSEMBLE DE TOUTES LES PROPOSITIONS FAITES PAR LE JOUEUR JUSQU'ICI
-$index          = 0;       // INDEX DU MOT À TROUVER DANS LE DICTIONNAIRE DICO
+$index = -1;       // INDEX DU MOT À TROUVER DANS LE DICTIONNAIRE DICO
 
 $word = "";       // LE MOT À TROUVÉ CHOISI ALÉATOIREMENT DANS LE DICO ET RETROUVÉ À CHAQUE TOUR GRÂCE À L'INDEX
 $clueString     = '';       // LA CHAÎNE À AFFICHER AU JOUEUR POUR LUI INDIQUER L'ENPLACEMENT DES LETTRES DÉJÀ TROUVÉES (LES LETTRES NON TROUVÉES SONT REPLACÉES PAR DES '_')
@@ -15,8 +15,6 @@ $nbErrors       = 0;        // NOMBRE D'ERREURS FAITES JUSQU'ICI PAR LE JOUEUR (
 $lost           = false;    // TRUE SI LE JOUEUR A PERDU
 $won            = false;    // TRUE SI LE JOUEUR A GAGNÉ
 $clueCSSClass   = '';       // CLASSE CSS À AJOUTER À LA CLUESTRING ON FONCTION DE LA LONGUEUR DU MOT (POUR QUE CELA RENTRE DANS L'ÉCRAN)
-
-// *********************
 
 // *********************
 
@@ -31,44 +29,36 @@ if (isset($_POST['lettre'])){
     if (strlen($lettre)>1){
         // VIDER LA VARIABLE LETTRE
         $lettre = '';
-    }    
-    // FIN SI
-
+    }  
+   
     // SI LA LETTRE NE FAIT PAS PARTIE DES LETTRES AUTORISÉES (ALPHABET)
     if (!preg_match("/^[a-zA-Z]$/", $lettre)) {
     
         // VIDER LA VARIABLE LETTRE
         $lettre = '';
     }    
-    // FIN SI
-// FIN SI
-
-
+    
 // SI L'INDEX DU MOT A TROUVER EXISTE DANS $_POST
 if (isset($_POST['index'])) {
     // RÉCUPÉRER L'INDEX DANS UNE VARIABLE
     $index = $_POST['index'];
 }
-// FIN SI
+
 
 // SI "PROPOSITIONS" EXISTE DANS $_POST
 if (isset($_POST['propositions'])) {
     // RÉCUPÉRER "PROPOSITIONS" DANS UNE VARIABLE
     $propositions = $_POST['propositions'];
 }
-// FIN SI
+
  
 // SI ON COMMENCE LA PARTIE (index est égal à -1)
 if ($index === -1) {
-    // CHOISIR UN MOT AU HASARD EST LE STOCKER DANS UNE VARIABLE
-    $word = getRandomWord();
-    // RECUPERER L'INDEX DU MOT ET LE STOCKER DANS UNE VARIABLE
-    $index = getIndexOfWord($word);
-    // ATTENTION, CETTE VARAIBLE DEVRA ÊTRE STOCKÉE DANS LE HTML SOU LA FORME D'UN INPUT TYPE HIDDEN
-    // POUR POUVOIR ÊTRE RETROUVÉ PLUS TARD LORS DES ROUNDS SUIVANTS
-}    
-// FIN SI
-
+// CHOISIR UN MOT AU HASARD EST LE STOCKER DANS UNE VARIABLE
+$word = getRandomWord();
+// RECUPERER L'INDEX DU MOT ET LE STOCKER DANS UNE VARIABLE
+$index = getIndexOfWord($word);
+}   
 
 // LA LETTRE QUE L'UTILISATEUR VIENT DE SAISIR N'EST PAS DÉJÀ DANS LES PROPOSITIONS PRÉCÉDENTES
 if (strpos($propositions, $lettre) === false) {
@@ -76,13 +66,11 @@ if (strpos($propositions, $lettre) === false) {
     $propositions .= $lettre;
 }
 
-// FIN SI
-
-
 // RÉCUPÉRER LE MOT DANS LE DICO EN UTILISANT L'INDEX
 $word = DICO[$index]; 
+d($word);
 // CRÉER LA "CLUESTERING" EN REMPLACANT, DANS LE MOT, LES LETTRES NON TROUVÉES PAR DES '_' (APPEL À LA FONCTION getClueString() )
-$clueString = getClueString($word, $propositions);
+$clueString = getClueString($propositions, $word);
 // RÉCUPÉRER LE NOMBRE D'ERREURS DU JOUEUR (APPEL À LA FONCTION countErrors() )
 $nbErrors = countErrors($word, $propositions);
 
@@ -97,7 +85,7 @@ if ($nbErrors >= 6) {
 // SI IL N'Y A PLUS DE '_' DANS LA CLUESTRING
 if (strpos($clueString, '_') === false) {
     // ON A GAGNÉ, METTRE LA BONNE VARIABLE À TRUE
-    $won = false;
+    $won = true;
 }
 // FIN SI
 
