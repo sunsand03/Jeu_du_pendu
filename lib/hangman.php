d6c267e598a0d7ea669afae6ccdb190aa82f931a
Sandrine IBRAIMA-NOMBO <sandrineibraima@gmail.com>
2023-08-22 12:09:41 +0200

    finalization of functions

--- lib/hangman.php
+++ lib/hangman.php
@@ -1,9 +1,9 @@
 <?php
 
-// charge le fichier de débug
+// loads the debug file
 require_once 'debug.php';
 
-// Dictionnaire contenant la liste des mots à deviner
+// Dictionary containing the list of words to guess
 const DICO = [
     'carabistouille',
     'calembredaine',
@@ -18,13 +18,29 @@ const DICO = [
     'paracétamol',
     'cucurbitacées',
     'coloquinte',
-    'vernaculaire'
+    'vernaculaire',
+    'xylophone',
+    'awalé',
+    'coccyx',
+    'whisky',
+    'constitution',
+    'cornée', 
+    'rubéfier',
+    'épigone',
+    'laudatif',
+    'ocelle',
+    'pétrichor',
+    'hypoténuse',
+    'ouroboros',
+    'mélopée',
+    'syllogisme',
+    'cordiforme'
 ];
 
 /**
- * Renvoie un mot choisi aléatoirement dans le  DICO
+ * Returns a word randomly selected in the DICO
  *
- * @return string Un mot du dictionnaire DICO
+ * @return string A word from the DICO 
  */
 function getRandomWord(): string
 {
@@ -33,45 +49,41 @@ function getRandomWord(): string
     return $word;
 }
 
-$word = getRandomWord();
-d($word);
-
 /**
- * Renvoie la position d'un mot dans le DICO
+ * Returns the position of a word in the DICO
  *
- * @param string $word Le mot à chercher dans le DICO
- * @return integer L'index du mot dans le DICO
+ * @param string $word The word to find in the DICO
+ * @return integer The index of the word in the DICO
  */
 function getIndexOfWord(string $word): int|false
 {
     $index = array_search($word, DICO);
     return $index;
 }
-$index = getIndexOfWord($word);
-d($index);
+
 
 /**
- * Renvoie le nombre de lettres erronées dans les propositions du joueurs
+ * Returns the number of wrong letters in the player’s proposals
  * 
- * @param string $propositions Toutes les lettres tapées par le joueur jusqu'ici
- * @param string $word Le mot à trouver
- * @return integer Le nombre de lettres erronées
+ * @param string $propositions All letters typed by the player so far
+ * @param string $word The word to find
+ * @return integer The number of wrong letters
  */
 function countErrors(string $word, string $propositions): int
 {
-    // retire les accents contenus dans $word et $propositions et les mets en minuscule
+    // removes accents from $word and $propositions and lowercase
     $word = mb_strtolower(iconv('UTF-8', 'ASCII//TRANSLIT', $word));
     $propositions = mb_strtolower(iconv('UTF-8', 'ASCII//TRANSLIT', $propositions));
 
-    // Initialise $errors à 0
+    // Initializes $errors to 0
     $errors = 0;
 
-    // Convertit $propositions en tableaux de lettres
+    // Converts $proposals to letter tables
     $propositionsArray = str_split($propositions);
 
-    // Pour chaque lettre dans les propositions
+    // For each letter in the proposals
     foreach ($propositionsArray as $letter) {
-        // Si la lettre n'est pas dans le mot
+        // If the letter is not in the word
         if (strpos($word, $letter) === false) {
             $errors++;
         }
@@ -81,50 +93,83 @@ function countErrors(string $word, string $propositions): int
 }
 
 /**
- * Renvoie une chaîne représentant les lettres déjà trouvées dans le mot
- * Chaque lettre non trouvée est remplacée par un '_' (undescore)
+ * Returns a string representing letters already found in the word
+ * Each letter not found is replaced by a '_' (undescore)
  * Ex : 
- * getClueString('aer','partie') // Renvoie '_ar__e'
- * getClueString('aeup','pause') // Renvoie 'pau_e'
- * ATTENTION ! IL VA, LA AUSSI, FALLOIR TROUVER COMMENT RETIRER TOUS LES ACCENTS À TROUVER 
+ * getClueString('aer','partie') // Return '_ar__e'
+ * getClueString('aeup','pause') // Return 'pau_e' 
  *
- * @param string $propositions Les lettres tapées par le joueur jusqu'ici
- * @param string $word Le mot à trouver
- * @return string La chaîne d'indices finale
+ * @param string $propositions The letters typed by the player so far
+ * @param string $word The word to find
+ * @return string The final chain of indices
  */
 function getClueString(string $propositions, string $word): string
 {
-    // Retire les accents contenus dans $word 
-    $word = iconv('UTF-8', 'ASCII//TRANSLIT', $word);
+    //stores the length of the word to guess 
+    $originalLength = mb_strlen($word, 'UTF-8');
+    
+    // A correspondence table to remove accents
+    $transliterationArray = [
+        'é' => 'e',
+        'è' => 'e',
+        'ê' => 'e',
+        'ë' => 'e',
+        'à' => 'a',
+        'ù' => 'u',
+        'ü' => 'u',        
+        'ï' => 'i',
+        'ö' => 'o'    
+    ];
+    //replaces special characters if found
+    $wordWithoutAccents = strtr($word, $transliterationArray);
 
-    // Initier une chaîne avec des underscores correspondant à la longueur du mot
-    $clueString = str_repeat('_', strlen($word));
+    // represents each character of the word to be found by an underscore
+    $clueString = str_repeat('_', $originalLength);
 
-    // Pour chaque lettre dans le mot à trouver $word
-    for ($i = 0; $i < strlen($word); $i++) {
-        // Si la lettre est dans les propositions
-        if (strpos($propositions, $word[$i]) !== false) {
-            // Remplacer l'underscore par la lettre correspondante au mot indice
-            $clueString[$i] = $word[$i];
+    // for each character
+    for ($i = 0; $i < $originalLength; $i++) {
+        //Get part of string
+        $character = mb_substr($word, $i, 1, 'UTF-8');
+        $characterWithoutAccent = mb_substr($wordWithoutAccents, $i, 1, 'UTF-8');
+
+        //if match found, replace the undercore with the character
+        if (strpos($propositions, $characterWithoutAccent) !== false) {
+            $clueString = mb_substr_replace($clueString, $character, $i, 1, 'UTF-8');
         }
     }
 
-    // Renvoyer le mot indice
     return $clueString;
 }
+
+/**
+ * Takes a multibyte string $string and replaces a substring within it with another string $replacement
+ *
+ * @param [type] $string The original string to modify
+ * @param [type] $replacement The string to be inserted instead of the substring
+ * @param [type] $start The starting index from which the substring begins to be repla
+ * @param [type] $length The length of the substring to be replaced
+ * @param [type] $encoding The character coding
+ * @return void
+ */
+function mb_substr_replace($string, $replacement, $start, $length = null, $encoding = null) {
+    // specify a character encoding if null
+    if ($encoding === null) {
+        $encoding = mb_internal_encoding();
+    }
+
+    // defines the length of $length if null
+    if ($length === null) {
+        $length = mb_strlen($string, $encoding) - $start;
+    }    
+
+    // return the replaced chain
+    return mb_substr($string, 0, $start, $encoding) . $replacement . mb_substr($string, $start + $length, null, $encoding);
+}    
+
+
 
 
 
  
 
-// //tests
-// $word = getRandomWord();
-// // $word = "encracer";
-// echo "mot à deviner :". $word . "\n";
-// // $index = getIndexOfWord($word);
-// // echo "index du mot dans le dico : ".$index . "\n";
-// $propositions = "enrace";
-// $nb_errors = countErrors($propositions,$word);
-// echo " Il y a ". $nb_errors. " erreurs". "\n";
-// $test = getClueString($propositions,$word);
-// echo $test . "\n"; 
+ 
